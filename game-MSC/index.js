41859a32e9a4b3ecdd01eebc599f16af5f23fe12
Edgar <Edgar@AnotherFoxGuy.com>
2019-12-22 14:16:57 +0100

    :sparkles: Added My Summer Car

---
+++ game-MSC/index.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const { util } = require('vortex-api');
+const os = require("os");
+
+
+function findGame() {
+  return util.steam.findByName('My Summer Car')
+    .then(game => game.gamePath);
+}
+
+function findModPath() {
+  return os.homedir() + '/Documents/MySummerCar/Mods';
+}
+
+function main(context) {
+  context.registerGame({
+    id: 'MSC',
+    name: 'My Summer Car',
+    logo: 'header.jpg',
+    mergeMods: true,
+    queryPath: findGame,
+    queryModPath: () => './Mods',
+    executable: () => 'mysummercar.exe',
+    requiredFiles: [
+      'mysummercar.exe',
+      'MSCLoader.dll'
+    ],
+    details: {
+      steamAppId: 516750,
+    },
+  });
+
+  return true;
+}
+
+module.exports = {
+  default: main,
+};
